9107636c735f5d34f2076385350d710d1a8250d1
S D Rausty <27742457+SDRausty@users.noreply.github.com>
2019-12-10 22:47:51 -0500

    Update ma.bash

--- ma.bash
+++ ma.bash
@@ -7,7 +7,7 @@ set -Eeuo pipefail
 shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212 "${0##*/} ma.bash" 
 cd "$JDR"
-# Usage: _AT_ login/repository commit
+# Usage: _AT_ account/repository commit
 _AT_ madisp/divide-and-conquer 60678a006c234d0c55a7ef6554f2755201b25841
 _AT_ twuster/Rubix 51be3c6c808ff95d0533ad77deac088c2af49f80
 # ma.bash OEF
